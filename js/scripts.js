828f11fb5196ae149436d8232ea36dece3ea182e
Gabriel Abraham. Singbeh <68817096+gabriel524@users.noreply.github.com>
2022-01-25 21:51:11 +0100

    First major code for building pokemon application with HTML, CSS and JavaScript.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,63 @@
+//initial declearation of the pokemon
+let PokemonList= [
+  {
+    name:'Bulbasaur',
+    height: 0.6,
+    type:  ['grass' , 'posion']
+  },
 
-alert('Hello world');
+  {
+    name: 'Nidoqueen',
+    height: 0.5,
+    type: ['Poison-point', 'Rivalry', 'Sheer-force']
+  },
 
-let name = prompt("What is your name?");
+  {
+    name: 'rattata',
+    height: 0.4,
+    type:['sucker punch' , 'blizzard']
+  },
 
-// array of strings
-let favoriteFood = ['rice', 'orange', 'berries'];
-let addfavoriteFood = (favoriteFood) + ('banana.');
-document.write(addfavoriteFood);
+  {
+    name:'Blastoise',
+    height:0.3,
+    type: ['Rain-dish,' , 'Torrent']
+  },
+
+  {
+    name: 'pikachu',
+    height: 0.5,
+    type: ['thundershock' , 'thunder']
+  },
+
+  {
+    name:'Fearow',
+    height: 0.7,
+    type: ['Keen-eye,' , 'Sniper']
+  },
+
+  {
+    name:'Charmeleon',
+    height: 1.1,
+    type: ['Blaze,' , 'Solar-power']
+  }
+
+]
+
+  //added function to return the "pokemonList"
+  function getAll() {
+    return pokemonList;
+  }
+
+
+console.log(PokemonList);
+
+// In this This Loop + conditional we specify that the pokemon with a height less than 1 should have the message appearing
+
+for (var i = 0; i < PokemonList.length; i++) {
+ if (PokemonList[i].height <= 2.0) {
+    document.write('<P>' +  PokemonList[i].name + ( ', height: ' )+ PokemonList[i].height + ( " (This is\ a good game !!)") + '<P>');
+ } else {
+   document.write('<P>' +  PokemonList[i].name + ( ', height: ' )+ PokemonList[i].height + '<P>')
+  }
+}
