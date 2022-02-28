3b59aaf562ff9ad650fc7d3dc49f6d0c37dfce4a
Gabriel Abraham. Singbeh <68817096+gabriel524@users.noreply.github.com>
2022-02-28 22:26:04 +0100

    pokemon array remove and a url and function for sorting all the pokemon added to the js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,198 +1,9 @@
 let pokemonRepository = (function () {
 
 //initial declearation of the pokemon
-let pokemonList = [
-  {
-    name:'Bulbasaur',
-    height: 0.6,
-    type:  ['grass' , 'posion']
-  },
+let pokemonList = [];
+let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
-  {
-    name: 'Nidoqueen',
-    height: 0.5,
-    type: ['Poison-point' , 'Rivalry' , 'Sheer-force']
-  },
-
-  {
-    name: 'Rattata',
-    height: 0.4,
-    type:['sucker punch' , 'blizzard']
-  },
-
-  {
-    name:'Blastoise',
-    height:0.3,
-    type: ['Rain-dish' , 'Torrent']
-  },
-
-  {
-    name: 'Pikachu',
-    height: 0.5,
-    type: ['thundershock' , 'thunder']
-  },
-
-  {
-    name:'Fearow',
-    height: 1.6,
-    type: ['Keen-eye' , 'Sniper']
-  },
-
-  {
-    name:'Charmeleon',
-    height: 0.7,
-    type: ['Blaze' , 'Solar-power']
-
-  },
-
-  {
-    name:'Snorlax',
-    height: 2.11,
-    type: ['Normal']
-
-  },
-
-  {
-    name:'Charizard',
-    height: 5.07,
-    type: ['fire' , 'flying']
-
-  },
-
-  {
-    name:'Squirtle',
-    height: 1.08,
-    type: ['Water']
-
-  },
-
-  {
-    name:'Vivillon',
-    height: 3,
-    type: ['Bug', 'Flying']
-
-  },
-
-  {
-    name:'Weedle',
-    height: 1.00,
-    type: ['Bug' , 'Posion']
-
-  },
-
-  {
-    name:'Metapod',
-    height: 2.04,
-    type: ['Bug']
-
-  },
-
-  {
-    name:'Lucario',
-    height: 1.19,
-    type: ['Fighting' , 'Steel']
-
-  },
-
-  {
-    name:'Garchomp',
-    height: 1.9,
-    type: ['Ground' , 'Dragon']
-
-  },
-
-  {
-    name:'Eevee',
-    height: 30.5,
-    type: ['Normal']
-
-  },
-
-  {
-    name:'Gyarados',
-    height: 6.5,
-    type: ['Flying' , 'Water']
-
-  },
-  {
-    name:'Mew',
-    height: 40.6,
-    type: ['Psychic']
-
-  },
-
-  {
-    name:'Gastrodon',
-    height: 88.9,
-    type: ['Water' , 'Ground']
-
-  },
-
-  {
-    name:'Greninja',
-    height: 1.5,
-    type: ['Dark' , 'Water']
-
-  },
-
-  {
-    name:'Gardevoir',
-    height: 1.6,
-    type: ['Psychic' , 'Fairy']
-
-  },
-
-  {
-    name:'Togekiss',
-    height: 8.7,
-    type: ['Fairy' , 'Flying']
-
-  },
-
-  {
-    name:'Mewtwo',
-    height: 0.7,
-    type: ['Dark', 'Bug', 'Ghost']
-
-  },
-  {
-    name:'Budew',
-    height: 20.3,
-    type: ['Grass', 'Poison']
-
-  },
-  {
-    name:'Riolu',
-    height: 71.1,
-    type: ['Fighting']
-
-  },
-  {
-    name:'Palkia',
-    height: 4.19,
-    type: ['Water', 'Dragon']
-
-  },
-  {
-    name:'Roselia',
-    height: 30.5,
-    type: ['Grass' , 'Poison']
-
-  },
-  {
-    name:'Quagsire',
-    height: 1.4,
-    type: ['Ground' , 'Water']
-
-  },
-
-  {
-    name:'Torterra',
-    height: 2.21,
-    type: ['Grass' , 'Ground']
-
-  }
-];
 //addedin a function that add new pokemen to the "pokemonList"
 function add(pokemon) {
   //checking for only certain properties to be accepted when new pokemon is added
@@ -230,12 +41,32 @@ function addListItem(pokemon) {
     });
   }
 
+function loadList() {
+    return fetch(apiUrl).then(function (response) {
+      return response.json();
+    }).then(function (json) {
+      json.results.forEach(function (item) {
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+      });
+    }).catch(function (e) {
+      console.error(e);
+    })
+  }
+
+
+
+
 // A return statement that return all the given pokemon
 return {
   add: add,
   getAll: getAll,
   addListItem: addListItem,
-  showDetails: showDetails
+  showDetails: showDetails,
+  loadList: loadList
 
 };
 
@@ -244,9 +75,11 @@ return {
 console.log(pokemonRepository.getAll());
 
 // forEach loop to iterate over the pokemon in pokemonList
+pokemonRepository.loadList().then(function() {
 pokemonRepository.getAll().forEach(function(pokemon){
   pokemonRepository.add(pokemon);{
   pokemonRepository.addListItem(pokemon);
   pokemonRepository.showDetails(pokemon);
 };
+});
 });
