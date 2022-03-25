9ab6fc44ed91094a1aabcdf5cf3fec0b79b2732a
Gabriel Abraham. Singbeh <68817096+gabriel524@users.noreply.github.com>
2022-03-25 18:17:47 +0100

    Bootstrap modal further edited for displaying all details of the pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -58,71 +58,67 @@ function loadList() {
   })
 }
 
+//load data of each pokemon when click on pokemon
 function loadDetails(item) {
   let url = item.detailsUrl;
   return fetch(url).then(function (response) {
     return response.json();
   }).then(function (details) {
-    /// Now we add the details to the item
-      item.imageUrl = details.sprites.front_default;
-      item.height = details.height;
-      item.weight = details.weight;
-      item.types = details.types;
-      item.abilities = [];
-      for (let i = 0; i < details.abilities.length; i++) {
-        item.abilities.push(details.abilities[i].ability.name);
-      }
-    }).catch(function (e) {
-      console.error(e);
-    });
-  }
-  //called when a user clicks on a pokemon button; gets pokemon details from the server
-function showDetails(pokemon) {
-    loadDetails(pokemon).then(function () {
-      console.log(pokemon);
-      showModal(pokemon.name, pokemon.height, pokemon.imageUrl);
-    });
-  }
-
+    // Now we add the details to the item
+    item.imageUrl = details.sprites.front_default;
+    item.height = details.height;
+    item.weight = details.weight;
+    item.types = [];
+    for (let i = 0; i < details.types.length; i++) {
+      item.types.push(details.types[i].type.name);
+    }
+    item.abilities = [];
+    for (let i = 0; i < details.abilities.length; i++) {
+      item.abilities.push(details.abilities[i].ability.name);
+    }
+  }).catch(function (e) {
+    console.error(e);
+  });
+}
+//after click on pokemon button,load the data of pokemon from server
+function showDetails(item) {
+  loadDetails(item).then(function () {
+    showModal(item);
+  });
+}
 function findAllPokemon(searchName) {
    // Clear all the buttons on the page when user types in search box
    $(".pokemon-list").empty();
  }
+
 function showModal(item) {
   // showModal function
   let modalTitle = $('.modal-title'); // modalTitle
   let modalBody = $('.modal-body'); // modalBody
-  let modalHeader = $('.modal-header')
   // let modalHeader = $(".modal-header"); // no header so removed
-  modalTitle.empty();
-  modalBody.empty();
 
-  let nameElement = $('<h1>' + item.name + '</h1>');
+  let pokemonName = $('<h1>' + item.name + '</h1>');
 
-  let imageElementFront = $('<img class="modal-img" style="width:50%">');
-
-  imageElementFront.attr('scr', item.imageUrlFront);  // Front of pokemon image attribute loaded from 'item.imageUrl'
-  let imageElementBack = $('<img class="modal-img" style="width:50%">');
-
-  imageElementBack.attr('scr', item.imageUrlBack); // Back of pokemon image attribute loaded from 'item.imageUrl'
-  // creating element for the height of the modal content
   let pokemonHeight = $('<p>' + 'Height: ' + item.height + '</p>');
 
   let pokemonWeight = $('<p>' + 'Weight: ' + item.weight + '</p>');
+  console.log(item.types);
 
   let pokemonTypes = $('<p>' + 'Types: ' + item.types + '</p>');
 
   let pokemonAbilities = $('<p>' + 'Abilities: ' + item.abilities + '</p>');
 
+  let pokemonImage = $('<img class="modal-image" style="width:35%">');
+  pokemonImage.attr('src', item.imageUrl); // pokemon image attribute loaded from 'item.imageUrl'
+
   modalTitle.empty(); // clears the modalTitle after display
   modalBody.empty(); // clears the modalBody after display
 
-  modalTitle.append(nameElement); // pokemonName is displayed as the title in the modal
-  modalBody.append(imageElementFront); // pokemonImage is displayed in the body of the modal
-  modalBody.append(imageElementBack); // pokemonHeight is displayed in the body of the modal
+  modalTitle.append(pokemonName); // pokemonName is displayed as the title in the modal
+  modalBody.append(pokemonImage); // pokemonImage is displayed in the body of the modal
   modalBody.append(pokemonHeight); // pokemonHeight is displayed in the body of the modal
-  modalBody.append(pokemonWeight);
-  modalBody.append(pokemonTypes);// pokemonWeight is displayed in the body of the modal
+  modalBody.append(pokemonWeight); // pokemonWeight is displayed in the body of the modal
+  modalBody.append(pokemonTypes);
   modalBody.append(pokemonAbilities); // pokemonDetails are displayed in the body of the modal
 }
   // A return statement that return all the given pokemon
